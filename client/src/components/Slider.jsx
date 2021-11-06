0620a81e9e188e21b1c8345f2b959cd84ebf354b
Marios Tsioutsis <13-13029@saeintitute.edu>
2021-11-06 18:08:46 +0200

    Init homepage

---
+++ client/src/components/Slider.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectFade } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+
+const imgStyles = {
+  display: 'block',
+  objectFit: 'cover',
+  objectPosition: 'center',
+  width: '100%',
+  height: 'auto',
+};
+
+const Slider = ({ images, styles }) => {
+  return (
+    <Swiper
+      modules={[Autoplay, EffectFade]}
+      style={{ ...styles }}
+      speed={1200}
+      spaceBetween={0}
+      slidesPerView={1}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      effect='fade'
+    >
+      {images.map((slide) => (
+        <SwiperSlide>
+          <img src={slide} style={{ ...imgStyles }} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Slider;
