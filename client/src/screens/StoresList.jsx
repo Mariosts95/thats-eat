b53d36de7b71e36178493b825fcd5d3136657c1a
Marios Tsioutsis <13-13029@saeintitute.edu>
2022-01-10 21:39:24 +0200

    Add stores filter funcionality

--- client/src/screens/StoresList.jsx
+++ client/src/screens/StoresList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 import { UseStores } from '../store/StoreProvider';
 
@@ -6,25 +6,52 @@ import './StoresList.scoped.scss';
 import StoreCard from '../components/StoreCard';
 
 const StoreList = () => {
-  const { storesList, storesLoading } = UseStores();
+  const { storesList, storesLoading, cuisines } = UseStores();
+  const [filter, setFilter] = useState();
+  const getCuisine = (e) => {
+    if (e.target.value == 'all') setFilter('');
+    setFilter(e.target.value);
+  };
 
-  if (storesLoading || !storesList) return <div>Loading...</div>;
+  if (storesLoading || !storesList || !cuisines) return <div>Loading...</div>;
 
   return (
     <div className='stores-list'>
-      <h2>{storesList.count} Available Stores:</h2>
-      {storesList.stores.map((store) => (
-        <StoreCard
-          key={store._id}
-          id={`/stores/${store._id}`}
-          name={store.name}
-          cuisines={store.cuisines}
-          banner={store.imgPath}
-          imgAlt={store.imgAlt}
-          rating={store.rating}
-          minDelivery={store.minDelivery}
-        />
-      ))}
+      <div className='stores-top-container'>
+        <div className='title'>
+          <h2>{storesList.count} Available Stores:</h2>
+        </div>
+        <div className='filters'>
+          <label>Filter: </label>
+          <select name='cuisines' id='cuisines' onChange={getCuisine}>
+            <option key='all' value='all'>
+              All
+            </option>
+            {cuisines.map((cuisine) => (
+              <option key={cuisine._id} value={cuisine._id}>
+                {cuisine.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      {storesList.stores
+        .filter((store) => {
+          if (filter == 'all') return true;
+          return store.cuisines.includes(filter);
+        })
+        .map((store) => (
+          <StoreCard
+            key={store._id}
+            id={`/stores/${store._id}`}
+            name={store.name}
+            cuisines={store.cuisines}
+            banner={store.imgPath}
+            imgAlt={store.imgAlt}
+            rating={store.rating}
+            minDelivery={store.minDelivery}
+          />
+        ))}
     </div>
   );
 };
